2e2b8cf67b70929488b01b2cd2a2e511c645cd48
Saadkhan0105 <saadkhan9560@gmail.com>
2024-12-22 15:53:13 +0530

    Node Modules

---
+++ 02_Modules/main.js
@@ -0,0 +1,15 @@
+// import {a, b, d} from "./myModule.js"
+// console.log(a, b, d)
+
+// import harry from "./myModule.js"
+// console.log(saad)
+
+// (function(exports, require, module, __filename, __dirname) {
+
+//     // Module code actually lives here
+
+//   });
+
+const a = require("./myModule2.js");
+
+console.log(a, __dirname, __filename);
